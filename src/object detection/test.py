ed64856b1063eeddf8ff644639045bf60093053a
ad15042 <yudai.onep@gmail.com>
2023-05-03 20:29:21 +0900

    Second commit at 20230503

---
+++ src/object detection/test.py
@@ -0,0 +1,17 @@
+# a = next(i for i in range(1,10)
+#          if i == 3)
+# print(a)
+
+import os, time, uuid
+
+# for i in range(10):
+#     print(i)
+#     if i > 6:
+#         exit(-1)
+
+
+# print(uuid.uuid4())
+
+# a = base_image_location = os.path.join (os.path.dirname(__file__), "images/base_images")
+# print(a)
+
